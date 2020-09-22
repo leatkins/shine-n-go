534f1701bc01813770285f2d0caa4171a6d790ec
LeRon Atkins <leatkins@aboveall-media.tech>
2020-09-22 11:22:23 -0500

    modifying port number

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000;
 const fs = require('fs')
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
